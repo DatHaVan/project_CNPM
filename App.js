d6b683210b6e597e53cf4c0c2343dacd15d07c1a
DatHaVan <154257233+DatHaVan@users.noreply.github.com>
2023-12-19 13:21:07 +0700

    App.js

--- App.js
+++ App.js
@@ -1,1 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState, useRef} from 'react';
+import { StyleSheet,Modal,Text,Image,TextInput, View ,TouchableOpacity, ImageBackground} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Alert } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './Home';
+import Login from './Login';
+import KhaiSinh from './KhaiSinh';
+import Khaibao from './Khaibao';
+
+const Stack = createNativeStackNavigator();
+export default function App() {
+  return ( 
+ <NavigationContainer>
+ <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
+   <Stack.Screen name="Login" component={Login} />
+   <Stack.Screen name="Home" component={Home} />
+   <Stack.Screen name="KhaiSinh" component={KhaiSinh} />
+   <Stack.Screen name="Khaibao" component={Khaibao} />
+ </Stack.Navigator>
+</NavigationContainer> 
+ );
+};
 
